50fddd547914c3f369e2c8546b4dc73b6781be60
Natron Kan <waterbeside@163.com>
2023-05-07 12:50:10 +0800

    init: 从@lunisolar/sx拆分到新项目

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+export const J2000 = 2451545 // 2000年1月1日的UT12:00是儒略日2451545
+
+export const GRE_UNITS = {
+  ms: 'millisecond',
+  s: 'second',
+  m: 'minute',
+  h: 'hour',
+  d: 'day',
+  w: 'week',
+  M: 'month',
+  y: 'year'
+}
+
+export const REGEX_PARSE =
+  /^(\d{4})[-/]?(\d{1,2})?[-/]?(\d{0,2})[Tt\s]*(\d{1,2})?:?(\d{1,2})?:?(\d{1,2})?[.:]?(\d+)?$/
---
+++ typings/types.ts
@@ -0,0 +1,36 @@
+export type DateDict = {
+  year: number
+  month: number
+  day: number
+  hour: number
+  minute: number
+  second: number
+  millis?: number
+}
+
+export type JDConfig = {
+  isUTC: boolean
+  offset: number
+}
+
+export type LunarMonth = {
+  isLeap: boolean
+  len: number
+  month: number
+  dayJdn: number
+}
+
+export type GreUnitFullName =
+  | 'millisecond'
+  | 'second'
+  | 'minute'
+  | 'hour'
+  | 'day'
+  | 'week'
+  | 'month'
+  | 'quarter'
+  | 'year'
+
+export type GreUnitShortName = 'ms' | 's' | 'm' | 'h' | 'd' | 'w' | 'M' | 'q' | 'y'
+
+export type GreUnit = GreUnitFullName | GreUnitShortName
