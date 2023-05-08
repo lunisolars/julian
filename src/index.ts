cbf775576e21bb8db9d65978b751655ff3b9118e
Natron Kan <waterbeside@163.com>
2023-05-08 13:15:26 +0800

    init: exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { JD } from './jd'
+
+export { date2DateDict, parseDateString, prettyUnit, jdn2gre, gre2jdn } from './utils'
+export { REGEX_PARSE, J2000, GRE_UNITS } from './constants'
+
+export type { JDConfig, GreUnitFullName, GreUnitShortName, GreUnit } from '../typings/types'
