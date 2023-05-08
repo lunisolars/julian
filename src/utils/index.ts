b46acedf742e89806b4509add636c21477c3aa87
Natron Kan <waterbeside@163.com>
2023-05-08 12:51:10 +0800

    feat: jdn2gre

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -33,7 +33,15 @@ export function date2DateDict(date?: Date | Partial<DateDict> | number): DateDic
   }
 }
 
-export function gre2jdn(date?: Date | Partial<DateDict>, isUTC = false) {
+/**
+ * Gregorian calendar to Julian Day Number
+ * 公历转儒略日数
+ *
+ * @param date Gregorian calendar date
+ * @param isUTC is UTC
+ * @returns Julian Day Number
+ */
+export function gre2jdn(date?: Date | Partial<DateDict>, isUTC = false): number {
   const dateDict = date2DateDict(date) as DateDict
   const now = new Date()
   let year = dateDict?.year ?? now.getFullYear()
@@ -57,6 +65,61 @@ export function gre2jdn(date?: Date | Partial<DateDict>, isUTC = false) {
   if (month <= 2) (month += 12), year--
   if (G) (n = int2(year / 100)), (n = 2 - n + int2(n / 4)) //加百年闰
   return int2(365.25 * (year + 4716)) + int2(30.6001 * (month + 1)) + day + n - 1524.5 + dig
+}
+
+/**
+ * Juilan Day Number to Gregorian calendar
+ * @param jdn Julian Day Number
+ * @param isUTC is UTC
+ * @returns  Gregorian calendar date dict
+ */
+export function jdn2gre(jdn: number, isUTC = false): Required<DateDict> {
+  if (!isUTC) {
+    const timezoneOffset = -new Date().getTimezoneOffset()
+    jdn += timezoneOffset / (24 * 60)
+  }
+  //儒略日数转公历
+  const r: Required<DateDict> = {
+    year: 0,
+    month: 0,
+    day: 0,
+    hour: 0,
+    minute: 0,
+    second: 0,
+    millis: 0
+  }
+  let D = int2(jdn + 0.5),
+    F = jdn + 0.5 - D,
+    c //取得日数的整数部份A及小数部分F
+  if (D >= 2299161) {
+    c = int2((D - 1867216.25) / 36524.25)
+    D += 1 + c - int2(c / 4)
+  }
+  D += 1524
+  r.year = int2((D - 122.1) / 365.25) //年数
+  D -= int2(365.25 * r.year)
+  r.month = int2(D / 30.601) //月数
+  D -= int2(30.601 * r.month)
+  r.day = D //日数
+  if (r.month > 13) {
+    r.month -= 13
+    r.year -= 4715
+  } else {
+    r.month -= 1
+    r.year -= 4716
+  }
+  //日的小数转为时分秒
+  F *= 24
+  r.hour = int2(F)
+  F -= r.hour
+  F *= 60
+  r.minute = int2(F)
+  F -= r.minute
+  F *= 60
+  r.second = int2(F)
+  F -= r.second
+  r.millis = F * 1000
+  return r
 }
 
 /**
