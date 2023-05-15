39083b67d3b528c701d76b59a97c5919ca8698fb
Natron Kan <waterbeside@163.com>
2023-05-15 18:11:03 +0800

    export utils

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+export {
+  dateDict2jdms,
+  jdms2hms,
+  modDayMs,
+  gre2jdn,
+  jdn2DateDict,
+  jdn2timestamp,
+  timestamp2jdn,
+  jdDict2timestamp,
+  timestamp2jdDict
+} from '@lunisolar/utils'
