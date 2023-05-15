e160b7f627a322776ff95cb70dbea5dc7f622948
Natron Kan <waterbeside@163.com>
2023-05-15 18:45:45 +0800

    export jdn2dateDict

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { JD } from './jd'
 
-export { date2DateDict, string2DateDict, prettyUnit, jdn2gre, gre2jdn } from '@lunisolar/utils'
+export { date2DateDict, string2DateDict, prettyUnit, jdn2dateDict, gre2jdn } from '@lunisolar/utils'
 export { REGEX_PARSE, J2000, GRE_UNITS } from './constants'
 
 export type { JDConfig, GreUnitFullName, GreUnitShortName, GreUnit } from '../typings/types'
